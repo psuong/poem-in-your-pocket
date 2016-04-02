3340ae8619e3d63a4ebc0ce92621d86460a79cd0
exp0nge <mdislamwork@gmail.com>
2016-04-03 00:44:49 +0100

    Fixed Word.objects.create

--- pocket_poem/poem/buzzfeed.py
+++ pocket_poem/poem/buzzfeed.py
@@ -7,7 +7,9 @@ import string
 
 os.environ.setdefault("DJANGO_SETTINGS_MODULE", "pocket_poem.settings")
 
-from poem.models import Word
+from poem.models import Word, Category
+from poem.word_processing import syllable_count, part_of_speech
+from poem.categories import PARTS_OF_SPEECH
 
 SECTIONS = ['fwd', 'lol', 'win', 'omg', 'cute', 'trashy', 'fail', 'wtf', 'books', 'travel', 'music', 'animals', 'lgbt',
             'sports', 'politics', 'tech', 'entertainment', 'health', 'diy', 'food', 'rewind', 'celebrity', 'comedy',
@@ -22,6 +24,11 @@ PUNCTUATIONS = set(string.punctuation)
 
 def clean_html(raw_html):
     return ''.join(ch for ch in re.sub(CLEAN_REGEX, '', raw_html) if ch not in PUNCTUATIONS)
+
+
+def populate_categories():
+    for category in PARTS_OF_SPEECH:
+        Category.objects.get_or_create(name=category)
 
 
 def populate_words(buzz_section=None):
@@ -41,9 +48,13 @@ def populate_words(buzz_section=None):
             for sub_buzz in sub_buzzes:
                 for word in clean_html(sub_buzz['description']).split(' '):
                     if not word == word.upper() and word:
-                        if not Word.objects.filter(text=word.lower()).exists():
-                            # if the found word is not in the database, store it
-                            print word
+                        # store the word
+                        print 'Adding', word.lower()
+                        Word.objects.get_or_create(text=word.lower(),
+                                                   syllable_count=syllable_count(word.lower()),
+                                                   article_id=buzz['id'],
+                                                   category=part_of_speech(word.lower()))
 
 if __name__ == '__main__':
+    populate_categories()
     populate_words()
--- pocket_poem/poem/word_processing.py
+++ pocket_poem/poem/word_processing.py
@@ -1,11 +1,17 @@
+# coding=utf-8
 from nltk.tag import pos_tag
+from textstat.textstat import textstat
 
 parts_of_speech = ('article', 'noun', 'verb', 'adjective', 'adverb', 'preposition', 'conjunction')
 pos_dict = {'CC': 'conjunction', 'DT': 'article', 'IN': 'proposition', 'JJ': 'adjective', 'JJR': 'adjective',
-            'JJS': 'adjective', 'NN': 'noun', 'NNP': 'noun', 'NNPS': 'noun', 'NNS': 'noun', 'PRP': 'noun', 'PRP$': 'noun',
-            'RB': 'adverb', 'RBR': 'adverb', 'RBS': 'adverb', 'VB': 'verb', 'VBG': 'verb', 'VBN': 'verb', 'VPN': 'verb',
-            'VBZ': 'verb', 'WDT': 'article', 'WP': 'noun', 'WPS': 'noun', 'WRB': 'adverb'}
+            'JJS': 'adjective', 'NN': 'noun', 'NNP': 'noun', 'NNPS': 'noun', 'NNS': 'noun', 'PRP': 'noun',
+            'PRP$': 'noun', 'RB': 'adverb', 'RBR': 'adverb', 'RBS': 'adverb', 'VB': 'verb', 'VBG': 'verb',
+            'VBN': 'verb', 'VPN': 'verb', 'VBZ': 'verb', 'WDT': 'article', 'WP': 'noun', 'WPS': 'noun', 'WRB': 'adverb'}
 
 
-def pos_thing(word):
+def part_of_speech(word):
     return pos_dict[pos_tag(word)[0][1]]
+
+
+def syllable_count(word):
+    return round(textstat.syllable_count(word))
