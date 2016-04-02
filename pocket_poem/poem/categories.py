7acd9577a7c4ee93737d970256e69730912b89f2
exp0nge <mdislamwork@gmail.com>
2016-04-02 21:50:04 +0100

    Added basic word models

---
+++ pocket_poem/poem/categories.py
@@ -0,0 +1,8 @@
+# coding=utf-8
+PARTS_OF_SPEECH = (('articles', 'articles'),
+                   ('noun', 'noun'),
+                   ('verb', 'verb'),
+                   ('adjective', 'adjective'),
+                   ('adverb', 'adverb'),
+                   ('preposition', 'preposition'),
+                   )
--- pocket_poem/poem/models.py
+++ pocket_poem/poem/models.py
@@ -1,3 +1,13 @@
+# coding=utf-8
 from django.db import models
 
-# Create your models here.
+from poem import categories
+
+
+class Category(models.Model):
+    name = models.CharField(choices=categories.PARTS_OF_SPEECH, max_length=100)
+
+
+class Word(models.Model):
+    syllable_count = models.SmallIntegerField()
+    category = models.ForeignKey(Category)
