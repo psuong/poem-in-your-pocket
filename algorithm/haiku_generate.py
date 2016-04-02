6f3874a96c24d74f2a3f9478853505df63bd689e
Allen Kim <allenkim95@gmail.com>
2016-04-02 19:55:20 -0400

    Finished basic implementation of haiku

--- algorithm/haiku_generate.py
+++ algorithm/haiku_generate.py
@@ -1,42 +1,10 @@
-import random as rand
-from sentences import sentence_structures
+from line_generate import generate_line
 
-rand_sent_num = rand.randint(0,len(sentence_structures)-1)
+line1 = generate_line(5)
+print line1
+line2 = generate_line(7)
+print line2
+line3 = generate_line(5)
+print line3
 
-sentence_structure = sentence_structures[rand_sent_num]
-rand_sentence_structure = sentence_structure
-rand_sentence = []
-rand_index = []
-for x in xrange(0,len(rand_sentence_structure)):
-    rand_sentence_structure[x] = [rand_sentence_structure[x],x]
-
-rand.shuffle(rand_sentence_structure)
-for x in xrange(0,len(rand_sentence_structure)):
-    rand_index.append(rand_sentence_structure[x][1])
-    rand_sentence_structure[x] = rand_sentence_structure[x][0]
-
-remaining_pos = len(sentence_structure)
-remaining_syll = 5
-for generate_word in rand_sentence_structure:
-    wordTuple = generate_word()
-    word = wordTuple[0]
-    word_syll = wordTuple[1]
-    if remaining_pos == 1:
-        while word_syll != remaining_syll:
-            wordTuple = generate_word()
-            word = wordTuple[0]
-            word_syll = wordTuple[1]        
-    else:
-        while word_syll > (remaining_syll - remaining_pos + 1):
-            wordTuple = generate_word()
-            word = wordTuple[0]
-            word_syll = wordTuple[1]
-    
-    remaining_syll -= word_syll
-    remaining_pos -= 1
-    rand_sentence.append(word)
-sentence = range(len(rand_sentence))
-for x in xrange(0,len(rand_sentence)):
-    sentence[rand_index[x]] = rand_sentence[x]
-
-print " ".join(sentence)
+print "%s\n%s\n%s\n" % (line1,line2,line3)
