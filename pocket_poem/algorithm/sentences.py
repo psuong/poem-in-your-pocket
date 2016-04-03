aa20f74f498e445b19d1334728b780b3200fcaa3
Allen Kim <allenkim95@gmail.com>
2016-04-03 03:00:26 -0400

    Added some sentence templates

--- pocket_poem/algorithm/sentences.py
+++ pocket_poem/algorithm/sentences.py
@@ -48,17 +48,23 @@ def conjunction():
 def article():
     return generatePOS('article')
 
+# Generate a random pronoun
+def pronoun():
+    return generatePOS('pronoun')
 
 # List of all the different sentence structures
 # Repeated sentence structures are there to increase their likelihood of being chosen
 sentence_structures = [
     [article, noun, verb],
+    [article, adjective, noun],
+    [article, adjective, adjective, noun],
     [article, adjective, noun, verb],
     [article, noun, verb, adverb],
+    [pronoun, verb],
+    [pronoun, verb, noun],
     [adjective, noun, verb],
     [adjective, noun, verb, adverb],
     [article, noun, verb, article, noun],
-    [preposition, article, noun],
     [preposition, article, noun],
     [preposition, article, noun],
     [article, noun, conjunction, article, noun]
